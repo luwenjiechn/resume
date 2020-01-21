08cfff325d387a3b8f3ea1c5d72157d5a9a5f3af
luwenjie <luwenjie@kedacom.com>
2020-01-21 17:32:07 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Paper } from "@material-ui/core";
+import ReactMarkdown from "react-markdown";
 
 const App = () => {
+  const [markdown, setMarkdown] = useState("");
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("/resume.md");
+      const data = await res.text();
+      setMarkdown(data);
+    })();
+  }, []);
   return (
-    <Container fixed>
-      <Paper elevation={3}>
-        
+    <Container style={{ padding: 20 }} fixed>
+      <Paper elevation={3} style={{ padding: "20px" }}>
+        <ReactMarkdown source={markdown}></ReactMarkdown>
       </Paper>
     </Container>
   );
