86c1328cdb0988541cbedac0fd0deb9b3982a465
luwenjie <luwenjie@kedacom.com>
2020-01-21 17:10:30 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Paper } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <Container fixed>
+      <Paper elevation={3}>
+        
+      </Paper>
+    </Container>
+  );
+};
+
+export default App;
